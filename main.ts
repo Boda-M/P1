797a19d278b28937612d6192baf9290ec99323d1
UserID117 <78589673+UserID117@users.noreply.github.com>
2021-09-21 13:27:49 +0200

    V2 DrawNumber

--- main.ts
+++ main.ts
@@ -1,56 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 30; index++) {
+        DrawNumber(randint(1, 7))
+    }
+})
 function DrawNumber (szám: number) {
     if (szám == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+        led.plot(2, 2)
     } else if (szám == 2) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `)
+        led.plot(4, 0)
+        led.plot(0, 4)
     } else if (szám == 3) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
+        led.plot(0, 4)
+        led.plot(2, 2)
+        led.plot(4, 0)
     } else if (szám == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
+        led.plot(0, 0)
+        led.plot(4, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
     } else if (szám == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
+        led.plot(0, 0)
+        led.plot(4, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
+        led.plot(2, 2)
     } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+        led.plot(0, 0)
+        led.plot(0, 2)
+        led.plot(0, 4)
+        led.plot(4, 0)
+        led.plot(4, 2)
+        led.plot(4, 4)
     }
 }
 input.onGesture(Gesture.Shake, function () {
-    DrawNumber(randint(1, 7))
+	
 })
 basic.forever(function () {
 	
