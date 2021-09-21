c81d13eb8c6f0ea8689d5a7bc21e07cc1ea52c83
UserID117 <78589673+UserID117@users.noreply.github.com>
2021-09-21 13:07:01 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,57 @@
+function DrawNumber (szám: number) {
+    if (szám == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (szám == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (szám == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (szám == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (szám == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+}
+input.onGesture(Gesture.Shake, function () {
+    DrawNumber(randint(1, 7))
+})
+basic.forever(function () {
+	
+})
