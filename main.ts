0d7d17d905099263e7621c4fc4be3d6496399e6d
Boda-M <78589673+Boda-M@users.noreply.github.com>
2021-09-23 12:58:01 +0200

    Semmi

--- main.ts
+++ main.ts
@@ -1,19 +1,7 @@
 let Nr = 0
+let Running = 0
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 30; index++) {
-        Nr = randint(1, 7)
-        basic.clearScreen()
-        DrawNumber(Nr)
-        basic.pause(index * 12)
-        if (index == 30) {
-            for (let index2 = 0; index2 < 4; index2++) {
-                basic.pause(300)
-                basic.clearScreen()
-                basic.pause(300)
-                DrawNumber(Nr)
-            }
-        }
-    }
+    Roll()
 })
 function DrawNumber (szám: number) {
     if (szám == 1) {
@@ -45,9 +33,40 @@ function DrawNumber (szám: number) {
         led.plot(4, 4)
     }
 }
+function Értékel () {
+    Nr = 0
+    Running = 1
+    while (Running == 1) {
+        let index = 0
+        basic.clearScreen()
+        DrawNumber(Nr)
+        basic.pause(index * 12)
+        Nr += 1
+        if (6 <= Nr) {
+            Nr = 1
+        }
+        basic.pause(70)
+    }
+}
+function Roll () {
+    for (let index = 0; index <= 30; index++) {
+        Nr = randint(1, 7)
+        basic.clearScreen()
+        DrawNumber(Nr)
+        basic.pause(index * 12)
+        if (index == 30) {
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.pause(300)
+                basic.clearScreen()
+                basic.pause(300)
+                DrawNumber(Nr)
+            }
+        }
+    }
+}
 input.onGesture(Gesture.Shake, function () {
-	
+    Roll()
 })
-basic.forever(function () {
+input.onButtonPressed(Button.B, function () {
 	
 })
